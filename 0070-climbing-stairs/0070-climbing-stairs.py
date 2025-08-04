e012339df716286fc031794c2423f6360d82a1ba
Kenan Mengistu <101623653+Kenanmen@users.noreply.github.com>
2025-08-04 21:51:21 +0300

    Time: 0 ms (100%), Space: 17.7 MB (48.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n<= 3:
-            return n
-        else:
-            first = 1
-            second = 2
-            for i in range(3,n+1):
-                third = first + second
-                first = second
-                second = third
-            return second
-        
-        
+        mem = defaultdict(int)
+        def func(i):
+            if i <= 3:
+                return i
+            if not mem[i]:
+                mem[i] = func(i-1) + func(i-2)
+            return mem[i]
+        return func(n)
+            
+       
