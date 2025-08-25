f6270de3bfa27dcb4bbfd5bc4c6c92e335365524
Kenan Mengistu <101623653+Kenanmen@users.noreply.github.com>
2025-08-25 22:11:50 +0300

    Time: 6 ms (47.65%), Space: 17.8 MB (69.7%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        vals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        ans = []
+
+        for i in range(len(nums)):
+            while num >= nums[i]:
+                num -= nums[i]
+                ans.append(vals[i])
+       
+        return "".join(ans)
+        
+        
