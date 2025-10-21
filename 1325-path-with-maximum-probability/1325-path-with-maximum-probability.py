bb74e1427b22fc1d633dbf6b02f1ce2c54cfc187
Kenan Mengistu <101623653+Kenanmen@users.noreply.github.com>
2025-10-21 09:30:06 -0700

    Time: 138 ms (23.69%), Space: 29.1 MB (41.93%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], suc: List[float], start: int, en: int) -> float:
+        graph = defaultdict(list)
+        for idx,(l, r) in enumerate(edges):
+            graph[l].append((r,suc[idx]))
+            graph[r].append((l,suc[idx]))
+        dicts = defaultdict(lambda : float("-inf"))
+
+        dicts[start] = 1
+        heap = [(-1, start)]
+        visited = set()
+        while heap:
+            dist,node = heappop(heap)
+            dist *= -1
+            if node in visited:
+                continue
+            visited.add(node)
+            for ne,we in graph[node]:
+                update = we*dist
+                if update > dicts[ne]:
+                    dicts[ne] = update
+                    heappush(heap,(-1 *update,ne))
+        return dicts[en] if dicts[en] != float('-inf') else 0
+
+
+
+
+        
